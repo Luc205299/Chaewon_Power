99a9b54df1152bb2a42671cc317741a72493acfa
Luc205299 <94066059+Luc205299@users.noreply.github.com>
2024-10-19 17:21:56 +0200

    Surprise

--- chaewon.ps1
+++ chaewon.ps1
@@ -56,12 +56,68 @@ function Get-Chaewon {
         $global:form.Controls.Add($pictureBox)
         $global:form.ClientSize = $image.Size
         
-        # Use Show() instead of ShowDialog() to allow the script to continue
         $global:form.Show()
+
+        if($images  -eq 'Spam.jpg'){
+            Get-Spam-Chaewon -folderPath $folderPath
+        }
     } else {
         Write-Host "No images found in '$folderPath'."
     }
 }
+
+function Get-Spam-Chaewon {
+    param (
+        [string]$folderPath 
+    )
+
+    $images = Get-ChildItem -Path $folderPath -File
+    Write-Host "Looking for nice pictures in '$folderPath'..."
+    
+
+    # Load and display images in multiple windows
+    if ($images) {
+        Add-Type -AssemblyName System.Windows.Forms
+        Add-Type -AssemblyName System.Drawing
+
+        while($true) {
+            Start-Sleep -Seconds 1
+            $randomImage = $images | Get-Random
+            $form = New-Object Windows.Forms.Form
+            $form.Text = "Chaewon Spam <3 $i"
+            $form.StartPosition = "Manual"
+
+            # Set random positions on the screen for each window
+            $screenWidth = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Width
+            $screenHeight = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Height
+
+            $randomX = Get-Random -Minimum 0 -Maximum ($screenWidth - 300)
+            $randomY = Get-Random -Minimum 0 -Maximum ($screenHeight - 300)
+            $form.Location = New-Object Drawing.Point($randomX, $randomY)
+
+            # Load the image and resize it
+            $image = [System.Drawing.Image]::FromFile($randomImage.FullName)
+            $newWidth = $image.Width / 3
+            $newHeight = $image.Height / 3
+            $image = $image.GetThumbnailImage($newWidth, $newHeight, $null, [intptr]::Zero)
+
+            # Create PictureBox to display the image
+            $pictureBox = New-Object Windows.Forms.PictureBox
+            $pictureBox.Image = $image
+            $pictureBox.SizeMode = "AutoSize"
+            $pictureBox.Dock = "Fill"
+
+            $form.Controls.Add($pictureBox)
+            $form.ClientSize = $image.Size
+
+
+            $form.Show()
+        }
+    } else {
+        Write-Host "No images found in '$folderPath'."
+    }
+}
+
 
 function Close-Chaewon {
     if ($global:form) {
@@ -79,6 +135,8 @@ while ($true) {
     Start-Sleep -Seconds 3
     Close-Chaewon
     Get-Chaewon -folderPath $folderPath
+    
+    
 
     # If a key is pressed, exit the loop
     if ([System.Console]::KeyAvailable) {
